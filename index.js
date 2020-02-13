72b9d2d77b4062ccef93a73011648c1a9975e9ae
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-02-13 13:12:29 +0000

    created the add role  dm command

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Client } = require('discord.js'); //import Client from discord.js using object destructuring 
 const { config } = require('dotenv'); // import config  from dot env using object destructuring 
 
+//Role IDs
+const dm = '676465656228478990'
+const elven = '675133738157342739'
+
 //creating the bot client
 const client = new Client({
     disableEveryone: true
@@ -34,10 +38,6 @@ client.on('message', async message => {
     //console.log(`${message.author.username} said:${message.content}`)
     const prefix = "_";
 
-    //races
-    const elfRole = message.guild.roles.get("675133738157342739")
-    //const roleID = "675133738157342739";
-
     //to prevent infinate loops
     if (message.author.bot) return;
     if (!message.guild) return;
@@ -45,7 +45,6 @@ client.on('message', async message => {
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-
     //_say hello my name is sidsa
     //say
     //[hello, my, name, is, sidsa]
@@ -53,27 +52,39 @@ client.on('message', async message => {
     //ping
     if (cmd === 'ping') {
         const msg = await message.channel.send('pinging...');
-
+        //console.log(message.guild.roles)
         msg.edit(`Pong!\nLatency is ${Math.floor(msg.createdAt - message.createdAt)}ms \nApi Latency ${Math.round(client.ping)}ms`);
 
     }
-    //-------------------------------------------------Note--------------------------------------------
+    //---------------------------------Note ALL or Nothing issue--------------------------------------------
     //this is the point I've figured out discord will either edit the message for everyone or not at all 
-    //so i need to find another way
+    //so i need to find another way 
     if (cmd === 'elf') {
-        const msg = await message.channel.send('mumble mumble');
-
-        if (member.roles.has(elfRole)) {
-            msg.edit('testing')
-            console.log('I am here')
-        } else {
-            msg.edit(msg + '\nYou dont understand what they are saying')
-        }
+        const msg = await message.channel.send('Command reserved for now');
+    }
 
 
-        //(message.guild.roles === elfRole) translates
-        //(message.member.roles.has(elfRole)) does not
-
+    //changing the permissions on a user to grant them role of Dungeon Master(DM)
+    //USER:_dm paraic 
+    //bot:checks paraic has DM role
+    //      If yes, message to state it exists
+    //         else add DM role to user
+    if(cmd === 'dm') {
+        const role= message.guild.roles.get(dm)
+        const member = message.mentions.members.first()
+        if (!message.mentions.users.size) {
+            return message.channel.send('Please provide a name to premote to DM')
+        }else if (message.member.roles.has(dm)) {
+            return message.channel.send(`${member.displayName} already has the ${role.name} role!`)
+        } else {       
+            message.member.addRole(role)
+                .then(member => {
+                    message.channel.send(`Added role ${role.name} to ${member.displayName}`)
+                })
+                .catch(error =>{
+                console.log(error)
+                });
+        }        
     }
 });
 
